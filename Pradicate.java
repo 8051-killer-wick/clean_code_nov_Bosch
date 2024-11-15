566586fc6d20fbb66d2aff3e4bc9ecc67f1b5f86
NAGARJUN B M <82470011+8051-killer-wick@users.noreply.github.com>
2024-11-15 11:17:56 +0530

    Create Pradicate.java

---
+++ Pradicate.java
@@ -0,0 +1,30 @@
+package cleancode_java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class CompareStrings {
+
+  public static void main(final String[] args) {
+    new CompareStrings();
+  }
+
+  public CompareStrings() {
+    String[] names = { "Bosch", "Robert", "Bengalore", "Lenovo" };
+    IPradicateFunction pradicate = new PradicateFunction();
+    System.out.println(queryStrings(names, pradicate));
+  }
+
+  public static List<String> queryStrings(final String[] names, final IPradicateFunction pradicate) {
+    java.util.List<String> result = new ArrayList<>();
+    for (String name : names) {
+      if (pradicate.apply(name)) {
+        result.add(name);
+      }
+    }
+    return result;
+  }
+}
